30dd1ebe8b995c838724b5207133c8ef5a878060
toonnyy8 <toonnyy8@gmail.com>
2020-09-03 17:00:21 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -13,12 +13,48 @@ mod tui {
                                                                                       // original termios data
         buffer[0]
     }
+
+    pub struct RGBA {
+        pub r: u8,
+        pub g: u8,
+        pub b: u8,
+        pub a: u8,
+    }
+
+    pub fn pixel(upper: RGBA, lower: RGBA) -> String {
+        format!(
+            "\x1b[38;2;{};{};{}m\x1b[48;2;{};{};{}m\u{2580}\x1b[0m",
+            upper.r, upper.g, upper.b, lower.r, lower.g, lower.b
+        )
+    }
+    pub fn pixel_bottom(upper: RGBA) -> String {
+        format!(
+            "\x1B[38;2;{};{};{}m\u{2580}\x1B[0m",
+            upper.r, upper.g, upper.b
+        )
+    }
 }
 
 fn main() {
-    let mut c = 0;
-    while true {
-        c = tui::getch();
+    loop {
+        let c = tui::getch();
+        print!(
+            "{}",
+            tui::pixel(
+                tui::RGBA {
+                    r: c,
+                    g: 0,
+                    b: 0,
+                    a: 0
+                },
+                tui::RGBA {
+                    r: 0,
+                    g: c,
+                    b: 0,
+                    a: 0
+                }
+            )
+        );
         println!("{}", c as char);
         if c == 113 {
             break;
